7590af6edd96597d5f448f988889914b0355b51d
Joshua Wood <JerichoWall@apu.edu>
2019-01-24 12:44:16 -0800

    Partway through lambda-classes class creation

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,47 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(person) {
+        this.name = person.name;
+        this.age = person.age;
+        this.location = person.location;
+        this.gender = person.gender;
+    }
+    speak() {
+        console.log(`Hello, my name is ${this.name}, I am from ${this.location}.`);
+    }
+}
+
+
+class Instructor extends Person {
+    constructor(instruct) {
+        super(instruct);
+        this.specialty = instruct.specialty;
+        this.favLanguage = instruct.favLanguage;
+        this.catchPhrase = instruct.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
+
+
+class Student extends Person {
+    constructor(student) {
+        super(student);
+        this.previousBackground = student.previousBackground;
+        this.className = student.className;
+        this.favSubjects = student.favSubjects;
+    }
+    listsSubjects() {
+        console.log(`${this.favSubjects}`);
+    }
+    PRAassignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    }
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    }
+}
